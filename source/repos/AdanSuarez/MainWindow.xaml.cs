e290cf8677fea456f96920931718ab8caceb75aa
Adan1991 <Adasmovi@gmail.com>
2022-04-11 09:54:30 +0200

    commit

---
+++ source/repos/AdanSuarez/MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System.Windows;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdanSuarez
+{
+	/// <summary>
+	/// Lógica de interacción para MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Submit_Click(object sender, RoutedEventArgs e)
+		{
+			Dictionary<string, string> data = ReadFile();
+			if (!(data is null) && data["PRODUCT"] == "PRICE") //I would put this strings in proect resources
+			{
+				InsertOrUpdateFruits(data);
+			}
+			else if (!(data is null) && data["PRODUCT"]=="QUANTITY")
+			{
+				GenerateReceipt(data);
+			}
+			else
+			{
+				errormessage.Text = "Error. The file provided should start by PRODUCT,PRICE or PRODUCT,QUANTITY in the first line";
+			}
+
+		}
+
+		private void GenerateReceipt(Dictionary<string, string> data)
+		{
+			data = (Dictionary<string, string>)data.Where(x => x.Key != "PRODUCT");
+			float price = 0;
+			//The total price, the list of products purchased, the offers applied
+			StringBuilder sbF = new StringBuilder();
+			StringBuilder sbRecepit = new StringBuilder();
+			//I normally would put this in the database
+			sbF.Append(@"select Name,Price,ConditionSpent,ConditionQuantity,OfferDetail.ResultQuantity,ResultPrice,ResultQuantity,ResultFruitId from Fruit 
+						inner join FruitOffers on FruitOffers.IdFruit= Fruit.Id 
+						inner join OfferDetail on FruitOffers.IdOffer = OfferDetail.Id where Name in (");
+			foreach(string fruit in data.Keys)
+			{				
+				sbF.Append(fruit + ",");
+			}
+			sbF.Remove(sbF.Length - 1, 1);
+			sbF.Append(")");
+
+			string connectionString = Properties.Settings.Default.AdanSuarezConnectionString.ToString();
+			SqlConnection con = new SqlConnection(connectionString);
+			con.Open();
+			SqlCommand cmd = new SqlCommand(sbF.ToString(), con);
+			cmd.CommandType = CommandType.Text;
+			using (SqlDataReader reader = cmd.ExecuteReader())
+			{
+				if (reader.Read())
+				{ //I normally would have separated the string building and the price calculations and use variables for better understanding, but I started this way because I'm running out of time.
+					sbRecepit.AppendLine(reader["name"] + " : " + data[(string)reader["name"]] + "(" + reader["Price"] + ")");
+					price= (float)reader["Price"] *Int32.Parse((data[(string)reader["name"]]));
+					if (!(reader["conditionSpent"] is DBNull) && 
+						(((float)reader["Price"]) * Int32.Parse((data[(string)reader["name"]])) >= (float)reader["conditionSpent"])
+						||
+						(reader["conditionQuantity"] is DBNull) &&
+						(Int32.Parse((data[(string)reader["name"]])) >= (float)reader["conditionQuantity"])){
+						if (!(reader["ResultPrice"] is DBNull)){
+							price = price - (float)reader["ResultPrice"];
+							sbRecepit.AppendLine("Offer for that product (-" + (float)reader["ResultPrice"] + ")");
+						}
+						if (!(reader["ResultQuantity"] is DBNull))						{
+							sbRecepit.AppendLine("Offer for that product (a free fruit!" + reader["ResultFruitId"] + ")");
+						}
+					}
+				}
+				sbRecepit.AppendLine("----------");
+				sbRecepit.AppendLine("TOTAL: " + price);
+
+			}
+
+
+		}
+
+		private void InsertOrUpdateFruits(Dictionary<string, string> data)
+		{
+			try
+			{
+				data = (Dictionary<string, string>)data.Where(x => x.Key != "PRODUCT");
+				StringBuilder sb = new StringBuilder();
+				foreach (var fruit in data.Values)
+				{
+					string price = data[fruit];
+					//I would put this in the database or change the wat parameters are inserted but I don't have enough time
+					sb.Append(@"
+						begin tran 
+						if exists (select * from Fruit with (updlock,serializable) where Name = " + fruit + @")
+						begin
+						   update Fruit set Price = " + price + @"
+						   where name = " + fruit + @"
+						end
+						else
+						begin
+						   insert into fruit (name, price)
+						   values (" + fruit + ", " + price + @")
+						end
+						commit tran ");
+				}
+				string connectionString = Properties.Settings.Default.AdanSuarezConnectionString.ToString();
+				SqlConnection con = new SqlConnection(connectionString);
+				con.Open();
+				SqlCommand cmd = new SqlCommand(sb.ToString(), con);
+				cmd.CommandType = CommandType.Text;
+				cmd.ExecuteNonQuery();
+				con.Close();
+				errormessage.Text = "Product prices readed";
+			}
+			catch (Exception ex)
+			{
+				errormessage.Text = "Error reading Product Prices";
+			}
+
+
+		}
+
+		private Dictionary<string, string> ReadFile()
+		{
+			var ofd = new Microsoft.Win32.OpenFileDialog();
+			var result = ofd.ShowDialog();
+			if (result == false)
+			{
+				errormessage.Text = "Error reading the file.";
+				return null;
+			}
+			TextBoxFile.Text = ofd.FileName;
+			try
+			{
+				Dictionary<string,string> data = new Dictionary<string, string>();
+				using (var reader = new StreamReader(ofd.FileName))
+				{
+					string line;
+					while ((line = reader.ReadLine()) != null)
+					{
+						{
+							string name = line.Split(',')[0];
+							string number = line.Split(',')[1];
+							data.Add(name, number);
+						}
+					}
+				}
+				return data;
+			}
+			catch (Exception ex)
+			{
+				errormessage.Text = "Error reading the file.";
+				return null;
+			}
+
+		}
+	}
+}
---
+++ source/repos/AdanSuarez/views/Login.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+
+namespace AdanSuarez.views
+{
+	/// <summary>
+	/// Lógica de interacción para Login.xaml
+	/// </summary>
+	public partial class Login : Window
+	{
+		public Login()
+		{
+			InitializeComponent();
+		}
+        MainWindow registration = new MainWindow();
+        Welcome welcome = new Welcome();
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxEmail.Text.Length == 0)
+            {
+                errormessage.Text = "Enter an email.";
+                textBoxEmail.Focus();
+            }
+            else if (!Regex.IsMatch(textBoxEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                errormessage.Text = "Enter a valid email.";
+                textBoxEmail.Select(0, textBoxEmail.Text.Length);
+                textBoxEmail.Focus();
+            }
+            else
+            {
+                string email = textBoxEmail.Text;
+                string password = passwordBox1.Password;
+                string connectionString = Properties.Settings.Default.AdanSuarezConnectionString.ToString();
+
+                SqlConnection con = new SqlConnection(connectionString);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from Registration where Email='" + email + "'  and password='" + password + "'", con);
+                cmd.CommandType = CommandType.Text;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = cmd;
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                if (dataSet.Tables[0].Rows.Count > 0)
+                {
+                    string username = dataSet.Tables[0].Rows[0]["FirstName"].ToString() + " " + dataSet.Tables[0].Rows[0]["LastName"].ToString();
+                    welcome.TextBlockName.Text = username;//Sending value from one form to another form.  
+                    welcome.Show();
+                    Close();
+                }
+                else
+                {
+                    errormessage.Text = "Sorry! Please enter existing emailid/password.";
+                }
+                con.Close();
+            }
+        }
+        private void buttonRegister_Click(object sender, RoutedEventArgs e)
+        {
+            registration.Show();
+            Close();
+        }
+    }
+}  
+	
